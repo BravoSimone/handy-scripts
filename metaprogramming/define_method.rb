8d1b3d86fdd6900a3bb428a25670774eb947a95c
Simone Bravo <simonebravo96@gmail.com>
2016-12-03 10:42:13 +0100

    Add some comments

--- metaprogramming/define_method.rb
+++ metaprogramming/define_method.rb
@@ -1,3 +1,6 @@
+# In this module we're creating a kind of object oriented API, to do that we're using define_method taking method names
+# from an array. In this case I took the method's list from the floatsheduler's github page. If one day they will add
+# another resource most of the times I will only need to add another string in my AVALIABLE_CALLS array
 module Clients
   class Float
     BASE_URL = "https://api.float.com/api/v1"
--- metaprogramming/method_missing.rb
+++ metaprogramming/method_missing.rb
@@ -1,3 +1,9 @@
+# In this module we're creating a kind of object oriented API, to do that we're using method_missing and
+# respond_to_missing? taking method names from an array. In this case I took the method's list from the floatsheduler's
+# github page. If one day they will add another resource most of the times I will only need to add another string in my
+# AVALIABLE_CALLS array. This example is a little trickier because instead of defining all methods on the class
+# instance we take the method name and if it's present in our AVALIABLE_CALLS array we put that in the request uri,
+# otherwise we return the super class and make it handle the call
 module Clients
   class Float
     BASE_URL = "https://api.float.com/api/v1"
