4d09a802aa0c96c15b88fcc830c80e7daf99c9c7
Simone Bravo <simonebravo96@gmail.com>
2016-12-15 16:40:13 +0100

    Add timecop_module.rb

---
+++ rspec_utils/timecop_module.rb
@@ -0,0 +1,14 @@
+# Call rubocop directly from rspec metadata, example:
+# Rspec.describe 'what_to_test', timecop: { travel: Time.local(2016, 10, 15) } do
+AVALIABLE_METHODS = %w(freeze return scale travel).freeze
+
+module TimecopModule
+  RSpec.configure do |config|
+    config.around(:example, :timecop) do |example|
+      example.metadata[:timecop].each do |method, params|
+        Timecop.public_send(method, params) if AVALIABLE_METHODS.include? method.to_s
+      end
+      example.run
+    end
+  end
+end
